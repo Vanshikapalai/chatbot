e68577dddcf4c2db6ffa42f82e32490a0fd0a2f1
Vanshikapalai <vanshikapalai@gmail.com>
2025-12-22 23:20:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,28 +2,31 @@ from flask import Flask, request, jsonify, render_template
 import google.generativeai as genai
 import os
 
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+# Flask app
+app = Flask(__name__)
 
+# Gemini config
+genai.configure(api_key=os.environ.get("GOOGLE_API_KEY"))
 model = genai.GenerativeModel("gemini-1.5-flash")
 
-app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
 
-    @app.route("/chat", methods=["POST"])
+@app.route("/chat", methods=["POST"])
 def chat():
     data = request.get_json()
-    if not data or "message" not in data:
-        return jsonify({"reply": "No message received"}), 400
 
-    user_msg = data["message"]
+    user_message = data.get("message", "")
 
-    try:
-        response = model.generate_content(user_msg)
-        return jsonify({"reply": response.text})
-    except Exception as e:
-        return jsonify({"reply": f"Error: {str(e)}"}), 500
+    if user_message == "":
+        return jsonify({"reply": "Empty message"}), 400
 
+    response = model.generate_content(user_message)
+    return jsonify({"reply": response.text})
+
+
+if __name__ == "__main__":
+    app.run()
