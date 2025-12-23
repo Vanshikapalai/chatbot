7f7ac608df6f98507798a899876b1adf40a14b8c
Vanshikapalai <vanshikapalai@gmail.com>
2025-12-23 09:44:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,6 +23,30 @@ def chat():
         reply = "I am an AI-based chatbot designed for academic demonstration."
     else:
         reply = "That is an interesting question. I am designed to provide intelligent responses."
+        elif "python" in msg:
+    reply = "Python is a high-level programming language widely used for AI, web development, and data science."
+
+elif "flask" in msg:
+    reply = "Flask is a lightweight Python web framework used to build web applications and APIs."
+
+elif "chatbot" in msg:
+    reply = "A chatbot is a software application that simulates human conversation using predefined logic or AI models."
+
+elif "cloud" in msg:
+    reply = "Cloud computing allows applications to run on remote servers instead of local machines."
+
+elif "deployment" in msg:
+    reply = "Deployment is the process of making an application live so users can access it online."
+
+elif "render" in msg:
+    reply = "Render is a cloud platform used to deploy and host web applications."
+
+elif "technology" in msg:
+    reply = "Technology refers to the application of scientific knowledge to solve real-world problems."
+
+elif "future" in msg:
+    reply = "The future of AI includes automation, smart systems, and intelligent decision-making tools."
+
 
     return jsonify({"reply": reply})
 
